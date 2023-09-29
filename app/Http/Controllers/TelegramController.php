a5a3880eb5d9f7a2ac6d17368d9802b17a9dedb0
achmadtaufikhidayatullah <achmadtaufikhidayatullah6@gmail.com>
2023-09-29 15:34:10 +0800

    inisiasi BOT

---
+++ app/Http/Controllers/TelegramController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Gejala;
+use App\Models\Penyakit;
+use App\Models\PenyakitGejala;
+use App\Models\Diagnosa;
+use Carbon\Carbon;
+
+class TelegramController extends Controller
+{
+
+    use RequestTrait, MakeComponents;
+
+    private $mainMenu = [
+        ["Record Diagnosa Saya"],
+        ["Diagnosa Penyakit"],
+    ];
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $result = json_decode(file_get_contents('php://input'));
+        $action = $result->message->text;
+        $userId = $result->message->from->id;
+
+        
+        if($action == "/start") {
+            $text = "Selamat datang di Bot Telegram Diagnosis Penyakit Mata . Silahkan pilih menu di bawah ini: ";
+
+            $this->apiRequest('sendMessage', [
+                'chat_id' => $userId,
+                'text' => $text,
+                'reply_markup' => $this->keyboardBtn($this->mainMenu),
+            ]);
+
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::any('/' . env('TELEGRAM_BOT_TOKEN'), [App\Http\Controllers\TelegramController::class, 'index'])->name('webhook');
